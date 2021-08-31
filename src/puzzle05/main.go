4a4a7751df37f5dd0f7e7bf9fd9e42adbd2fec81
yifengyou <842056007@qq.com>
2021-08-31 22:09:56 +0800

    刷7题

--- src/puzzle05/main.go
+++ src/puzzle05/main.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
 
 //[primary] 关于字符串连接，下面语法正确的是（）
@@ -6,15 +7,16 @@ import "fmt"
 //B. str := "abc" + "123"
 //C. str ：= '123' + "abc"
 //D. fmt.Sprintf("abc%d", 123)
-//
-//参考答案：BD
 
 func main() {
-	 //str := ‘abc’ + ‘123’
+	//str := ‘abc’ + ‘123’ // 123 evaluated but not used
 	str := "abc" + "123"
-	//str := '123' + "abc"
-	 s:=fmt.Sprintf("abc%d", 123)
+	//str := '123' + "abc" // Too many characters in the rune literal
+	s := fmt.Sprintf("abc%d", 123)
+	// String printf return string type
 
-	fmt.Println(str)
-	fmt.Println(s)
+	fmt.Println(str) // abc123
+	fmt.Println(s) // abc123
 }
+
+//参考答案：BD
