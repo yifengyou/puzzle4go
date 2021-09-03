71fa48e770cf30920ab190ce1293b8449cb751eb
yifengyou <842056007@qq.com>
2021-09-03 10:05:29 +0800

    143这题与139一毛一样。。我上去就是给UP主一个mmp。。乱堆题目

--- src/puzzle142/main.go
+++ src/puzzle142/main.go
@@ -5,8 +5,12 @@ import (
 	"time"
 )
 
-//golang 两个协成交替打印1-100的奇数偶数
+//golang 两个协成 交替 打印1-100的奇数偶数
+
+
 var POOL = 100
+
+
 func groutine1(p chan int) {
 	for i := 1; i <= POOL; i++ {
 		p <- i
@@ -27,5 +31,9 @@ func main() {
 	msg := make(chan int)
 	go groutine1(msg)
 	go groutine2(msg)
-	time.Sleep(time.Second * 1)
+	time.Sleep(time.Second * 3)
 }
+
+// 考点
+
+// 无缓冲chan的基本使用，读端阻塞除非可读，写端阻塞除非可写
